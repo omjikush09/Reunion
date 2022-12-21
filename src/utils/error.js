f7064bfe30c04e8c49cd3c18cc731e4a0bb331e9
Omji Kushwaha <omjikush09@gmail.com>
2022-12-22 04:38:24 +0530

    fix:remove js files

--- src/utils/error.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorFunction = (res, error = "Something Went wrong", code = 501) => {
-    return res.status(code).json({ status: false, error });
-};
-exports.default = errorFunction;
